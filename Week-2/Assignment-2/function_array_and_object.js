dc81fce3291d8feea27b860e19404f456372f269
Joazen <johnsons544@gmail.com>
2023-09-04 11:01:51 +0800

    Assignment-2 done

---
+++ Week-2/Assignment-2/function_array_and_object.js
@@ -0,0 +1,27 @@
+function avg(data) {
+  // your code here
+  let priceSum = 0;
+  for (let i = 0; i < data.size; i++) {
+    priceSum += data.products[i].price;
+  }
+  return priceSum / data.size;
+}
+console.log(
+  avg({
+    size: 3,
+    products: [
+      {
+        name: "Product 1",
+        price: 100,
+      },
+      {
+        name: "Product 2",
+        price: 700,
+      },
+      {
+        name: "Product 3",
+        price: 250,
+      },
+    ],
+  })
+); // should print the average price of all products
