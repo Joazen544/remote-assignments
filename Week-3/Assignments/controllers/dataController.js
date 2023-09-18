cad10db0affa187cdc8ab8c9536e6c72056ddfcd
Joazen <johnsons544@gmail.com>
2023-09-18 19:19:19 +0800

    Fix week 3 Assignment 2 Wrong Answer and check no query

--- Week-3/Assignments/controllers/dataController.js
+++ Week-3/Assignments/controllers/dataController.js
@@ -1,15 +1,15 @@
 module.exports = (req, res, next) => {
-  if (Object.keys(req.query).length === 0) {
+  if (req._parsedUrl.query === null) {
     //If there's no query
 
     res.send("Lack of Parameter");
   } else if (isNaN(req.query["number"]) || req.length > 1) {
     //If typeof query <number> isn't a number or exists more than one query
 
-    res.send("Wrong Parameter, you should enter a positive integer");
+    res.send("Wrong Parameter");
   } else if (req.query["number"] < 0 || req.query["number"] === "") {
     //If <number> is not a positive integer
-    res.send("Input should be a positive integer");
+    res.send("Wrong Parameter");
   } else {
     //Calculate sum from 1 to number
     const number = +req.query["number"];
